08abd4d0e2bd8b0e8ffc15315ed4e48ebdeaa4dc
박주현 <yeochi1201@gmail.com>
2024-11-08 20:26:54 +0900

    feature/ IOCP Socket Handle
    error exist / IOCP Handle Closed when thread create

--- CppServer/CppServer/Listener.h
+++ CppServer/CppServer/Listener.h
@@ -9,21 +9,49 @@
 
 #include <list>
 #include <iterator>
-#include<Windows.h>
+#include<windows.h>
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#define MAX_THREAD_CNT	8
+
+typedef struct Session {
+	SOCKET clientSocket;
+	char buffer[8192];
+}Session;
+
 
 class Listener {
 public:
+
+	std::list<SOCKET> socket_list;
+	CRITICAL_SECTION socket_cs;
 	Listener();
+	HANDLE IOCP_Handler;
+
 	void SendChattingMessage(char* pszParam, SOCKET clientSocket);
+	bool CloseServer();
+
+	//Handler Function
+	bool CloseSocketHandler(DWORD dwType);
+	//Thread
+	DWORD WINAPI CompleteThreadFunc(LPVOID param);
+	DWORD WINAPI AcceptThreadFunc(LPVOID param);
 private:
+	//Main Function
+	void StartProgram();
+
+	//Start ListenSocket Func
 	bool ResetWinsock();
 	bool CreateSocket();
 	bool BindPort();
-	bool AddClientSocket(SOCKET clientSocket);
-	bool InitCtrlHandler();
 	bool WaitingClient(SOCKET listenSocket);
-	SOCKET AcceptClient();
-	void ObserveClient();
+
+	void CloseClient(SOCKET clientSocket);
+	void CloseAllClient();
+	
+	//Handler
+	bool InitCtrlHandler();
+
+
 };
 #endif
